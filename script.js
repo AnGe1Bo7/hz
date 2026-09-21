b2a0d5f9a26db106f6130b55349218d4eec1393f
AnGe1Bo7 <glebpancenko320@gmail.com>
2026-09-21 18:11:48 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-
+const buttons = document.querySelectorAll('#\6');
+buttons.forEach(function(button) {
+  button.addEventListener('click', function() {
+     gtag('event', 'click_start_btn');
+    
+});
